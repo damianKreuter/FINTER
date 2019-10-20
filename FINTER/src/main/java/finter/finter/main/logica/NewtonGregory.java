236d31339d6081b185f56503cf255781c37e4da2
damianKreuter <damiankreuter@outlook.com>
2019-10-20 17:41:58 -0300

    agregoElOtroAlgoritmoPeroLaBase

---
+++ FINTER/src/main/java/finter/finter/main/logica/NewtonGregory.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finter.finter.main.logica;
+
+import java.util.Collection;
+
+/**
+ *
+ * @author Propietario
+ */
+public class NewtonGregory implements Algoritmo{
+
+    @Override
+    public String realizarCalculo(Collection<Integer> xs, Collection<Integer> ys) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<Respuesta> hacerCalculoDePares(Collection<Integer> xs, Collection<Integer> ys) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Double realizarCalculoEnPar(Double x1, Double y1, Double x2, Double y2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String convertirRespuestaAString(Collection<Respuesta> respuesta) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String convertirRespuestaUnitariaEnBaseASuIteracion(Respuesta respuestaUnitaria) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean noEstaVacio(Double dato) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
